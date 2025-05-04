50c4c22e1f7cda98938912cef53f5311a671b7ed
Allen-Ndlovu <ndlovuallen58@gmail.com>
2025-05-04 11:42:30 +0200

    Updated the task controller

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -1,21 +1,49 @@
 const Task = require('../models/Task');
 
 exports.getTasks = async (req, res) => {
-  const tasks = await Task.find();
-  res.json(tasks);
+  try {
+    const tasks = await Task.find({ createdBy: req.user._id }).populate('assignedTo', 'name email');
+    res.json(tasks);
+  } catch (err) {
+    res.status(500).json({ message: 'Server error' });
+  }
 };
 
 exports.createTask = async (req, res) => {
-  const task = await Task.create(req.body);
-  res.status(201).json(task);
+  const { title, description, deadline, assignedTo } = req.body;
+  try {
+    const task = new Task({ title, description, deadline, assignedTo, createdBy: req.user._id });
+    await task.save();
+    res.status(201).json(task);
+  } catch (err) {
+    res.status(500).json({ message: 'Server error' });
+  }
 };
 
 exports.updateTask = async (req, res) => {
-  const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true });
-  res.json(task);
+  try {
+    let task = await Task.findById(req.params.id);
+    if (!task) return res.status(404).json({ message: 'Task not found' });
+    if (task.createdBy.toString() !== req.user._id.toString())
+      return res.status(403).json({ message: 'Not authorized' });
+
+    task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    res.json(task);
+  } catch (err) {
+    res.status(500).json({ message: 'Server error' });
+  }
 };
 
 exports.deleteTask = async (req, res) => {
-  await Task.findByIdAndDelete(req.params.id);
-  res.json({ message: 'Task deleted' });
+  try {
+    const task = await Task.findById(req.params.id);
+    if (!task) return res.status(404).json({ message: 'Task not found' });
+    if (task.createdBy.toString() !== req.user._id.toString())
+      return res.status(403).json({ message: 'Not authorized' });
+
+    await task.remove();
+    res.json({ message: 'Task removed' });
+  } catch (err) {
+    res.status(500).json({ message: 'Server error' });
+  }
 };
